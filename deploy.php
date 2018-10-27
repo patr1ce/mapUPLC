4a45e92ce802be7542b96d642ce8b0b23972ccc7
maximesenza <maxime@happy-dev.fr>
2018-10-27 15:20:51 +0200

    version en map

---
+++ deploy.php
@@ -0,0 +1,7 @@
+Updating a05b576..8a17974
+Fast-forward
+ en/climate-map.html => climate-map.html |  18 +++++++++---------
+ en/img/icon/star.png                    | Bin 14581 -> 0 bytes
+ 2 files changed, 9 insertions(+), 9 deletions(-)
+ rename en/climate-map.html => climate-map.html (97%)
+ delete mode 100644 en/img/icon/star.png
