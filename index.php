c2009e84e655673ccddad2e44f88c75be64491f2
patr1ce <patrice.manset@gmail.com>
2018-11-03 23:52:34 +0100

    Fix js bug in MapManager

--- index.php
+++ index.php
@@ -14,6 +14,20 @@ if (!in_array($lang, $validValues, true))
 include('./assets/locales/'.$lang.'.php');
 ?>
 <script>
+function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
+
 require("./assets/locales/<?php echo $lang; ?>.js");
 </script>
 
