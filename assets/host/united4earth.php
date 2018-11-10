eccf1993f0452c79f375af6e0f440530ae71745d
maximesenza <maxime@happy-dev.fr>
2018-11-10 20:11:31 +0100

    dynamic css

--- assets/host/united4earth.php
+++ assets/host/united4earth.php
@@ -1,8 +1,8 @@
 <?php
 
 $looknfeel = array( 
-	"U4E_MAP_CSS_SHEET" => "assets/css/united4earth.css",
-	"C4C_MAP_CSS_SHEET" => "assets/css/citizens-for-climate.css",
+	"U4E_MAP_CSS_SHEET" => "./assets/css/united4earth.css",
+	"C4C_MAP_CSS_SHEET" => "./assets/css/citizens-for-climate.css",
 	
 	
 );
