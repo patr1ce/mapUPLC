90b5b57cf9ae64d0afcb13eaf56297ec8eecd478
maximesenza <maxime@happy-dev.fr>
2019-03-07 16:02:54 +0100

    climate strike events

--- index.php
+++ index.php
@@ -246,10 +246,10 @@ window.eventTypeFilters = [
     name: '<?php echo $locale["INDEX_EVENT1"];?>',
     id: 'FridaysForFuture'
   },
-  /*{
+  {
     name: '<?php echo $locale["INDEX_EVENT2"];?>',
     id: 'Marches'
-  },*/
+  },
   {
     name: '<?php echo $locale["INDEX_GROUPS"];?>',
     id: 'campaign-office'
