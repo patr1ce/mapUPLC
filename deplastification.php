37b87af871ea53c73f929c5bb1d778547f8d010d
maxime <maxime@happy-dev.fr>
2019-05-15 10:52:12 +0200

    testing deplastic map

--- deplastification.php
+++ deplastification.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="<?php echo substr($lang,0,2); ?>" lang="<?php echo substr($lang,0,2); ?>" dir="ltr" prefix="og: https://ogp.me/ns#">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo substr($lang,0,2); ?>" lang="<?php echo substr($lang,0,2); ?>" dir="ltr" prefix="og: http://ogp.me/ns#">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <script src="assets/js/jquery.min.js"></script>
 <script>
-function require(script) {
+  function require(script) {
     $.ajax({
         url: script,
         dataType: "script",
@@ -19,20 +19,17 @@ function require(script) {
     });
 }
 require("./assets/locales/<?php echo $lang; ?>.js");
-
 </script>
-
-<link rel="icon" href="./assets/img/logo.png" sizes="32x32" />
 <link rel="icon" href="./assets/img/logo.png" sizes="32x32" />
 <link rel="icon" href="./assets/img/logo.png" sizes="192x192" />
-<meta name="title" content="<?php echo $locale["META_TITLE"]; ?>">
-<meta name="description" content="<?php echo $locale["META_DESC"]; ?>">
+<meta name="title" content="">
+<meta name="description" content="Une carte pour trouver des intiatives pour agir sur les problèmes climatiques. Rejoignez des groupes et des évenements ctioyens prêt de chez vous en un clic !">
 <meta property="og:type" content="website" />
-<meta name="keywords" content="<?php echo $locale["META_KEYWORD"]; ?>">
-<meta property="og:image" content="https://united4earth.org/wp-content/uploads/climate-event-map-2.png" />
-<meta property="og:url" content="/" />
-<meta property="og:title" content="<?php echo $locale["META_OGTITLE"]; ?>"/>
-<meta property="og:description" content="<?php echo $locale["META_OGDESC"]; ?>"/>
+<meta name="keywords" content="carte, climat, unis pour le climat, uplc, marches">
+<meta property="og:image" content="https://united4earth.org/wp-content/uploads/carte-pour-le-climat.png" />
+<meta property="og:url" content="https://united4earth.org/fr/marche-climat/" />
+<meta property="og:title" content="Carte interactive des initiatives pour le climat"/>
+<meta property="og:description" content="Une carte pour trouver des intiatives pour agir sur les problèmes climatiques. Des groupes et des évenements prêt de chez vous en un clic."/>
 <link href='//api.tiles.mapbox.com/mapbox.js/v2.1.9/mapbox.css' rel='stylesheet' />
 <meta name="robots" content="index,follow">
 
@@ -42,15 +39,31 @@ require("./assets/locales/<?php echo $lang; ?>.js");
 <meta name="twitter:title" content="">
 <meta name="twitter:description" content="">
 <meta name="twitter:image" content="">
-<link rel="canonical" href="<?php echo $locale["INDEX_CANONICAL"]; ?>">
+<link rel="canonical" href="<?php echo $locale["PASSED_EVENT_CANONICAL"]; ?>">
+
 <?php
-if($lang!='en') {
-  echo '<link rel="alternate" hreflang="en-US" href="'.$locale["INDEX_HREFLANG_EN"].'">';
-}
-if($lang!='fr') {
-  echo '<link rel="alternate" hreflang="fr-FR" href="'.$locale["INDEX_HREFLANG_FR"].'">';
-}
-?>
+$url_path = $_SERVER['REQUEST_URI'];
+/*hreflang management*/
+if( preg_match("/fr\/marche-climat/", $url_path) == TRUE) {
+echo('<link rel="alternate" hreflang="en-US" href="'.$locale["PASSED_EVENT_HREFLANG"].'">');
+              /*Add echo line here for new languages*/
+        }
+    else if (preg_match('/\/climate-map/', $url_path) == TRUE) {
+      echo('<link rel="alternate" hreflang="fr-FR" href="'.$locale["PASSED_EVENT_HREFLANG"].'">');
+             /*Add echo line here for new languages*/
+             }
+
+    else  echo "<!-- none, something's wrong -->" ;
+  
+ echo "<!--".$_SERVER['REQUEST_URI']."-->" ;
+
+    ?>
+
+
+<!-- alternate
+<?php echo '<link rel="alternate" hreflang="FR_fr" href="'.$locale["PASSED_EVENT_HREFLANG"].'" />' ?> 
+-->
+
 
 <!-- shared css -->
 <link rel="stylesheet" type="text/css" href="./assets/css/events-map.css">
@@ -58,7 +71,7 @@ if($lang!='fr') {
 <!-- site specific css -->
 <?php
 if(!empty($config['additional_css'])) echo '<link rel="stylesheet" type="text/css" href="'.$config['additional_css'].'">';
-?> 
+?>
 
 <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Merriweather:400,900,700' />
 <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Lato:100,900,700,400,300' />
@@ -77,46 +90,27 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4VHRC7"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<section id='header' >
+<section id='header'>
   <div class='lato'>
-  
     <div class="main-title-area">
       <div style="margin-bottom: 4px;">
         <div id='spiel'>
           <strong><?php echo $locale["INDEX_HEADER1"]; ?></strong><br>
           <?php echo $locale["INDEX_SUBTITLE1"]; ?>
-
   
         </div>
       </div>
     </div> 
   
     <nav>
-      <ul id="nav" >
+      <ul id="nav">
        <!-- <a style="color: white;" href="./climate-map.html"><li class="nav-contribute">EN</a></li>-->
-        
-          <a href="<?php echo $locale['LINK_HOME']; ?>" class="contribute contribute-big"><li>
-            <?php echo $locale["INDEX_HOME"]; ?>
-        </li></a>
-
-          <a class="contribute contribute-big" href="<?php echo $locale["INDEX_CLP_LINK"]; ?>"><li><?php echo $locale["INDEX_CLP"]; ?>
-        </li></a>
-       
-          <a href="<?php echo $locale["INDEX_NEWEVENT_LINK"]; ?>" class="contribute contribute-big" target="_blank">
-            <li><?php echo $locale["INDEX_NEWEVENT"]; ?>
-        </li></a>
-        
-
-        <a class="contribute contribute-big"  href="<?php if($lang=='en-US') echo './past-events'; else echo './evenements-passes'; ?>">
-          <li><?php echo $locale["INDEX_PASTEVENT"]; ?>
-        </li></a>
-        <a style="color: white;" href="<?php echo $locale["INDEX_LANG_CHANGE_LINK"]; ?>"> <li class="nav-contribute">
-          <?php echo $locale["INDEX_LANG_CHANGE"]; ?>
-        </li></a>
-
+        <li class="nav-contribute"><a style="color: white;" href="./"><?php echo $locale["INDEX_CURRENTEVENT"]; ?></a></li>
+        <li><a href="../" class="contribute contribute-big"><?php echo $locale["INDEX_HOME"]; ?></a></li>
         <li>
-          <a href="https://twitter.com/share" class="social-media" data-url="" data-text="" data-related="" data-count="none"><img src="./assets/img/icon/twitter-new.png"> </a>&nbsp;&nbsp;<div class="fb-share-button" data-href="" data-layout="button">
-          <a href="javascript:fbShare()"><img src='./assets/img/icon/facebook-new.png' alt="facebook" /><span><?php echo $locale["INDEX_SHARE"]; ?></span></a></div>
+          <a href="https://twitter.com/share" class="twitter-share-button" data-url="" data-text="" data-related="" data-count="none"><?php echo $locale["INDEX_TWEET"]; ?></a>&nbsp;&nbsp;<div class="fb-share-button" data-href="" data-layout="button">
+          <a href="javascript:fbShare('', 'Fb Share', 'Facebook share popup', 'h', 520, 350)"><img src='./assets/img/icon/fb.png' alt="facebook" /><span><?php echo $locale["INDEX_SHARE"]; ?></span></a></div>
+  
         </li>
       </ul>
     </nav>
@@ -202,25 +196,26 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <footer>
         <div id='footer-area' >
           <h5><?php echo $locale["INDEX_HEADER2"]; ?></h5>
-          <p><img class="no-mobile" alt="People of the world together for Earth" style="margin-left:10%;border: 0px solid black" src="https://united4earth.org/wp-content/uploads/yak-earth-citizens.jpg"  width="80%" ></p>
-
         </div>
       </footer>
-    </div> 
+    </div>
 
- <!--
-  <div style="">
- <a id="click-ulule" href="https://fr.ulule.com/citizen-lobbying/" target="_blank" style="color: white;"><div style="margin: 10% auto 5% auto;width: 80%;background-color: #ea504e;padding: 2%;">
+<!--
+ <div style="">
+  <a id="click-ulule" href="https://fr.ulule.com/citizen-lobbying/" target="_blank" style="color: white;"><div style="margin: 10% auto 5% auto;width: 80%;background-color: #ea504e;padding: 2%;">
 <p style="vertical-align: bottom; text-align: center;font-weight: bolder;text-transform: uppercase;">J-2 FINANCEMENT LOBBY CITOYEN</a></p>
 </div>
-
+  <p><img class="no-mobile" alt="Logo United4Earth" style="margin-left:10%;border: 0px solid black" src="https://united4earth.org/wp-content/uploads/2018/10/french-video-gif-no-repeat.gif"  width="80%" ></p>
 </div>
--->
+    </div>
+-->  
   </article>
   <article id='map'>
     <div id='map-container'></div>
   </article>
 </section>
+
+
 <section id='campaign-offices'>
   <div class='viewport'>
     <h3 class='title'>Nearby Campaign Offices</h3>
@@ -237,12 +232,12 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script src='//dcxc7a0ls04u1.cloudfront.net/js/deparam.min.gz'></script>
 <script src='assets/js/mapbox'></script>
 <!-- <script src='/assets/js/leaflet-bouncer.js'></script> -->
-<script src='./assets/js/moment.min.js'></script>
+<script src='//dcxc7a0ls04u1.cloudfront.net/js/moment.min.js'></script>
 <script src='./assets/js/js-cookie.js'></script>
 <script src='./assets/js/MapManager.js'></script>
 <script type='text/javascript'>
 window.eventTypeFilters = [
-  {
+    {
     name: '<?php echo $locale["INDEX_EVENT1"];?>',
     id: 'FridaysForFuture'
   },
@@ -258,10 +253,10 @@ window.eventTypeFilters = [
     name: '<?php echo $locale["INDEX_EVENT3"];?>',
     id: 'trees'
   },
-   /* {
+    {
     name: '<?php echo $locale["INDEX_EVENT4"];?>',
     id: 'petition'
-  },*/
+  },
     /*{
     name: '<?php echo $locale["INDEX_EVENT5"];?>',
     id: 'fridayfuture'
@@ -277,11 +272,11 @@ window.eventTypeFilters = [
 var date = new Date();
 $("#loading-icon").show();
 $.ajax({
-  url: 'assets/d/deplastification.json',
+  url: 'assets/d/event-data-1310.json',
   dataType: 'script',
   cache: true, // otherwise will get fresh copy every page load
   success: function(data) {
-    d3.csv('assets/d/deplastification-global.csv',
+    d3.csv('assets/d/campaign-offices.csv',
       function(campaignOffices) {
         //Load zipcodes
         //dcxc7a0ls04u1.cloudfront.net
@@ -292,13 +287,13 @@ $.ajax({
           window.EVENT_DATA.results.forEach(function(d) {
             d.filters = [];
             //Set filter info
-            switch(d.event_type_name) {
+          switch(d.event_type_name) {
               case "FridaysForFuture": d.filters.push("FridaysForFuture"); break;
               case "Marches": d.filters.push("Marches"); break;
               case "trees": d.filters.push("trees"); break;
               case "petition": d.filters.push("petition"); break;
               //case "fridayfuture": d.filters.push("fridayfuture"); break;
-             case "office": d.filters.push("campaign-office"); break;
+              case "office": d.filters.push("campaign-office"); break;
               default: d.filters.push('other'); break;
             }
 
@@ -496,7 +491,7 @@ $("#filter-list").append(
     function fbShare(url, title, descr, image, winWidth, winHeight) {
         var winTop = (screen.height / 2) - (winHeight / 2);
         var winLeft = (screen.width / 2) - (winWidth / 2);
-        window.open('https://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
     }
 </script>
 </body>
