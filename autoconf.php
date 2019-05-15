d2bdfcc41e6552b34f17497b87b807709342c684
maxime <maxime@happy-dev.fr>
2019-05-15 11:19:46 +0200

    test different filters for plastic event

--- autoconf.php
+++ autoconf.php
@@ -33,9 +33,10 @@ if (!in_array($lang, $validValues, true))
   $lang=$config['default_locale'];
 
 if (strpos($_SERVER["REQUEST_URI"]), "deplastification") {
-	include('./locales/default/'."fr-FR-plastic".'.php');
-
-	else{
+	//include('./locales/default/'."fr-FR-plastic".'.php');
+	include('./locales/default/'.$lang.'.php');
+	
+	else {
 		include('./locales/default/'.$lang.'.php');
 	}
 }
